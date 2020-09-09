82fe14faecede5f7252f3a73046029f8f0ee08b6
wangzhiwei <942248412@qq.com>
2020-09-09 20:24:40 +0800

    init

---
+++ net_struct/ip.go
@@ -0,0 +1,30 @@
+package net_struct
+
+/**
+IP Header
+Version:协议版本。v4||v6
+HeaderLen:整个ip header的长度（非字节长度）。按32位（4字节）为一个长度单位（层）。因此最小值是5,因为header至少有5层，最多15层
+DS:区分服务，基本用不到
+TotalLen:总长度。实际上还是由MTU的1500bytes决定,超出时就需要分片传
+*/
+
+//单位bit(位)
+type bits struct {
+}
+
+type IP struct {
+	Version   [4]bits
+	HeaderLen [4]bits
+	DS        [8]bits
+	TotalLen  [16]bits
+	Next      *levelTwo
+}
+
+/**
+Identification:标识.数据报计数器.一个数据报被分片时，每片的该段相同
+Flag:标志
+*/
+type levelTwo struct {
+	Identification [16]bits
+	Flag           [3]bits
+}
---
+++ net_struct/mac.go
@@ -0,0 +1,19 @@
+package net_struct
+
+/**
+MAC Header
+数据报文最外层 最小64bytes 其中规定数据部分最大1500bytes 即整包最大1564bytes
+各机构制定的帧格式有异，此处进以ethernet II为例
+DAddressMac:目的mac		6bytes
+SAddressMac:源mac	6bytes
+ProtocolType:协议类型	2bytes
+Data:数据	46-1500bytes
+FCS:校验和。CRC算法校验	4bytes
+*/
+type Mac struct {
+	DAddressMac  [6]byte
+	SAddressMac  [6]byte
+	ProtocolType [2]byte
+	Data         []byte
+	FCS          [4]byte
+}
